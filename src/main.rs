756c19167e5beab7ec31504771201b59d4af0bba
Robert Plant <rob@robertplant.io>
2021-01-26 21:12:26 +0000

    Updated some docs

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,22 @@ use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Cli {
+    /// Device to search for
     search: String,
 
+    /// Should the search be case sensitive
     #[structopt(short, long)]
     case_sensitive: bool,
 
+    /// The field from `pactl list` that should be searched
     #[structopt(short = "s", default_value = "Description")]
-    key: String,
+    search_key: String,
 }
 
 fn main() -> CliResult {
     let args = Cli::from_args();
 
-    let response = match pulse::search(args.key, args.search, args.case_sensitive) {
+    let response = match pulse::search(args.search_key, args.search, args.case_sensitive) {
         Ok(id) => paswitch::set_source(id).unwrap(),
         Err(err) => err,
     };
